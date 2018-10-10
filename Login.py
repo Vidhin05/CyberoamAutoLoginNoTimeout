9dda022c80d04bcaa901ab524bf2b4583c59f8a6
Vidhin <ividhin@gmail.com>
2018-10-10 21:09:26 +0530

    Added features - logout by ctrl-c, login every 2 hours for long downloads

--- Login.py
+++ Login.py
@@ -1,43 +1,57 @@
+"""
+author: Vivek Gover and Vidhin Parmar
+A cyberoam client emulator that automatically logs in after every 2 hours.
+Enter your username and password before running the script.
+Press ctrl-c to logout.
+"""
+# This code runs in python3 only
+import signal
+import sys
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
-def login(username, password):
+def signal_handler(signal, frame):
+    logoff()
+    sys.exit(0)
 
+
+def login():
     payload = {
         'mode': '191',
         'username': username,
         'password': password
     }
-
     with requests.Session() as s:
         p = s.post('https://10.100.56.55:8090/httpclient.html', data=payload, verify=False)
         soup = BeautifulSoup(p.text, "xml")
-        check = 'You have successfully logged in'
         for i in soup.find_all('message'):
-                result = i.text
-                print(result)
-    return result
+            response = i.text
+            print(username + ": " + response)
+    return response
 
 
-def logoff(username):
-        payload = {'mode': 193, 'username': username}
-        with requests.Session() as n:
-            j = n.post('https://10.100.56.55:8090/httpclient.html', data=payload, verify=False)
-            soup = BeautifulSoup(j.text, "xml")
-            for i in soup.find_all('message'):
-                result = i.text
-                print(result)
+def logoff():
+    payload = {'mode': 193, 'username': username}
+    with requests.Session() as n:
+        j = n.post('https://10.100.56.55:8090/httpclient.html', data=payload, verify=False)
+        soup = BeautifulSoup(j.text, "xml")
+        for i in soup.find_all('message'):
+            response = i.text
+            print(username + ": " + response)
 
 
 if __name__ == "__main__":
-    user = input('Enter username\n')
-    passcode = input('Enter password\n')
-    result = login(user, passcode)
+    username = input('Enter username:\n')
+    password = input('Enter password:\n')
+    signal.signal(signal.SIGINT, signal_handler)
+    result = login()
 
-    if result == 'You have successfully logged in':
-        cmd = input('Do you want to log off ? (0/1)\n')
-        if cmd == '0':
-            logoff(user)
+    while result == 'You have successfully logged in':
+        time.sleep(7100)
+        result = login()
